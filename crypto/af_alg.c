df0e29c4097c900244df2fd2213fbd34635e4773
Vishnu Vardhan Reddy Naini <vishnu@vishnunaini.com>
2017-02-14 14:08:21 +0530

    Revert "crypto: af_alg - Forbid bind(2) when nokey child sockets are present"
    This reverts commit 001bdef5571e6a7dcad7a2313520f9f0db961452.

--- crypto/af_alg.c
+++ crypto/af_alg.c
@@ -130,16 +130,19 @@ EXPORT_SYMBOL_GPL(af_alg_release);
 void af_alg_release_parent(struct sock *sk)
 {
 	struct alg_sock *ask = alg_sk(sk);
-	unsigned int nokey = ask->nokey_refcnt;
-	bool last = nokey && !ask->refcnt;
+	bool last;
 
 	sk = ask->parent;
+
+	if (ask->nokey_refcnt && !ask->refcnt) {
+		sock_put(sk);
+		return;
+	}
+
 	ask = alg_sk(sk);
 
 	lock_sock(sk);
-	ask->nokey_refcnt -= nokey;
-	if (!last)
-		last = !--ask->refcnt;
+	last = !--ask->refcnt;
 	release_sock(sk);
 
 	if (last)
@@ -182,7 +185,7 @@ static int alg_bind(struct socket *sock, struct sockaddr *uaddr, int addr_len)
 
 	err = -EBUSY;
 	lock_sock(sk);
-	if (ask->refcnt | ask->nokey_refcnt)
+	if (ask->refcnt)
 		goto unlock;
 
 	swap(ask->type, type);
@@ -295,7 +298,6 @@ int af_alg_accept(struct sock *sk, struct socket *newsock)
 
 	if (nokey || !ask->refcnt++)
 		sock_hold(sk);
-	ask->nokey_refcnt += nokey;
 	alg_sk(sk2)->parent = sk;
 	alg_sk(sk2)->type = type;
 	alg_sk(sk2)->nokey_refcnt = nokey;
